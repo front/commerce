4a582338f6ca59c0660bbb2bcef04f4cb2eb3d2a
Roberto Ornelas <rob.ornelas@gmail.com>
2012-02-27 22:15:54 +0000

    Produktvisning content theming

--- sites/all/themes/allertheme/template.php
+++ sites/all/themes/allertheme/template.php
@@ -37,3 +37,53 @@ function allertheme_preprocess_html(&$vars) {
   // */
 
 }
+
+/**
+ * Theme callback: display prices in a table.
+ *
+ * From commerce_price_savings_formatter module.
+ */
+function allertheme_commerce_price_savings_formatter_formatter($vars) {
+  // Build an array of table rows based on the prices passed in.
+  $rows = array();
+
+  // exit if no prices
+  if (empty($vars['prices'])) {
+    return '';
+  }
+
+  // store prices count
+  $prices_count = count($vars['prices']);
+
+  // move price to 1st position
+  if (isset($vars['prices']['price'])) {
+    $vars['prices'] = array('price' => $vars['prices']['price']) + $vars['prices'];
+  }
+
+  // build rows
+  foreach ($vars['prices'] as $key => $value) {
+    switch ($key) {
+      case 'list':
+        $label = t('List price');
+        break;
+
+      case 'price':
+        $label = t('Price:');
+        break;
+
+      case 'savings':
+        $label = t('You save');
+        break;
+    }
+
+    $rows[] = array(
+      'data' => array(
+        array('data' => $label, 'class' => array('price-label')),
+        array('data' => $value, 'class' => array('price-amount')),
+      ),
+      'class' => array('commerce-price-savings-formatter-' . $key),
+    );
+  }
+
+  return theme('table', array('rows' => $rows, 'attributes' => array('class' => array('commerce-price-savings-formatter-prices', 'commerce-price-savings-formatter-prices-count-' . $prices_count))));
+}
