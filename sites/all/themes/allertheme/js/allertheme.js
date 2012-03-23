5da78e8df6d4f5c147e5aa91a27b846d0f673f91
Roberto Ornelas <rob@front.no>
2012-03-23 01:00:39 +0000

    Taxonomy pages theming + js

--- sites/all/themes/allertheme/js/allertheme.js
+++ sites/all/themes/allertheme/js/allertheme.js
@@ -18,6 +18,17 @@
                   });
                   $('.eq', view).equalHeight();
               }
+
+              if ($('.view-id-taxonomy_custom', context).length) {
+                  var view = $('.view-id-taxonomy_custom', context);
+
+                  $('.views-row', view).each(function() {
+                    // TODO: make this wrapper using fieldgroup on DS
+                    // once the wireframe / design is closed
+                      $('div.field:not(.field-name-field-display-product)', this).wrapAll('<div class="eq" />');
+                  });
+                  $('.eq', view).equalHeight();
+              }
             });
         }
     };
